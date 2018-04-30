a362153b731a384b2ec96aa05cbd884b5319bfe3
abhishek.goyal2 <abhi25goel@gmail.com>
2018-04-30 11:47:54 +0530

    Commit with testng remove appium test

---
+++ src/test/java/ToolsQA/RESTAPITest.java
@@ -0,0 +1,22 @@
+package ToolsQA;
+
+import org.testng.annotations.Test;
+
+public class RESTAPITest {
+
+	
+	@Test
+	
+	public void postJira()
+	{
+		System.out.println("post Jira test");
+
+	}
+	
+	@Test
+	public void deleteTwitter()
+	{
+		System.out.println("delete twitter");
+
+	}
+}
---
+++ src/test/java/ToolsQA/SeleniumTest.java
@@ -0,0 +1,22 @@
+package ToolsQA;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+
+	 //testxml file from maven
+	//how to execute all test cases from test folder -mvn test
+	@Test
+	public void BrowserAutomation()
+	{
+		System.out.println("BorwerAutomation ");
+
+	}
+	
+	@Test
+	public void elementsUi()
+	{
+		System.out.println("elements in UI");
+
+	}
+}
